cd76a44194841564c9a3e5849e82e5a53ac2b6c1
melodypapa <melodypapa@outlook.com>
2026-03-29 17:42:11 +0800

    feat(os): update parser for 10 new containers and missing attributes
    Add parser methods for all new containers. Update existing parser methods
    to read missing attributes from OsAlarm, OsApplication, OsTask, OsCounter.

--- src/eb_model/parser/os_xdm_parser.py
+++ src/eb_model/parser/os_xdm_parser.py
@@ -14,10 +14,16 @@ Implements:
 """
 import xml.etree.ElementTree as ET
 from ..models.eb_doc import EBModel
-from ..models.os_xdm import Os, OsAlarm, OsAlarmActivateTask, OsAlarmCallback, OsAlarmIncrementCounter, OsAlarmSetEvent, OsCounter, OsResource
+from ..models.os_xdm import Os, OsAlarm, OsAlarmActivateTask, OsAlarmCallback
+from ..models.os_xdm import OsAlarmIncrementCounter, OsAlarmSetEvent, OsCounter, OsResource
+from ..models.os_xdm import CommonPublishedInformation, PublishedInformation, OsHwIncrementer
+from ..models.os_xdm import OsEvent, OsSpinlock, OsPeripheralArea, OsOS, OsHooks
+from ..models.os_xdm import OsCoreConfig, OsAutosarCustomization
 from ..models.os_xdm import OsScheduleTable
-from ..models.os_xdm import OsTask, OsIsr, OsApplication, OsScheduleTableEventSetting, OsScheduleTableExpiryPoint, OsScheduleTableTaskActivation
-from ..models.os_xdm import OsScheduleTblAdjustableExpPoint, OsTaskAutostart, OsMicrokernel, MkMemoryProtection, MkMemoryRegion
+from ..models.os_xdm import OsTask, OsIsr, OsApplication, OsScheduleTableEventSetting
+from ..models.os_xdm import OsScheduleTableExpiryPoint, OsScheduleTableTaskActivation
+from ..models.os_xdm import OsScheduleTblAdjustableExpPoint, OsTaskAutostart
+from ..models.os_xdm import OsMicrokernel, MkMemoryProtection, MkMemoryRegion
 from ..parser.eb_parser import AbstractEbModelParser
 
 
@@ -62,6 +68,16 @@ class OsXdmParser(AbstractEbModelParser):
         self.read_os_applications(element, os)
         self.read_os_resources(element, os)
         self.read_os_microkernel(element, os)
+        self.read_common_published_information(element, os)
+        self.read_published_information(element, os)
+        self.read_os_hw_incrementer(element, os)
+        self.read_os_events(element, os)
+        self.read_os_spinlocks(element, os)
+        self.read_os_peripheral_areas(element, os)
+        self.read_os_os(element, os)
+        self.read_os_hooks(element, os)
+        self.read_os_core_configs(element, os)
+        self.read_os_autosar_customization(element, os)
 
     def read_os_task_autostart(self, element: ET.Element, os_task: OsTask):
         """Parse OsTaskAutostart configuration for a task."""
@@ -85,6 +101,9 @@ class OsXdmParser(AbstractEbModelParser):
             os_task.setOsTaskSchedule(self.read_value(ctr_tag, "OsTaskSchedule"))
             os_task.setOsTaskType(self.read_optional_value(ctr_tag, "OsTaskType"))
             os_task.setOsStacksize(int(self.read_value(ctr_tag, "OsStacksize")))
+            os_task.setOsMeasureMaxRuntime(self.read_optional_value(ctr_tag, "OsMeasure_Max_Runtime"))
+            os_task.setOsTaskUseHwFp(self.read_optional_value(ctr_tag, "OsTaskUse_Hw_Fp"))
+            os_task.setOsTaskCallScheduler(self.read_optional_value(ctr_tag, "OsTaskCallScheduler"))
 
             for resource_ref in self.read_ref_value_list(ctr_tag, "OsTaskResourceRef"):
                 os_task.addOsTaskResourceRef(resource_ref)
@@ -147,6 +166,9 @@ class OsXdmParser(AbstractEbModelParser):
         else:
             raise ValueError("Unsupported OsAlarmAction <%s>" % chc)
         os_alarm.setOsAlarmAction(os_alarm_action)
+
+        # Read callback name if available
+        os_alarm.setOsAlarmCallbackName(self.read_optional_value(element, "OsAlarmCallbackName"))
 
     def read_os_alarms(self, element: ET.Element, os: Os):
         """
@@ -233,7 +255,10 @@ class OsXdmParser(AbstractEbModelParser):
                 .setOsCounterTicksPerBase(self.read_value(ctr_tag, "OsCounterTicksPerBase")) \
                 .setOsCounterType(self.read_optional_value(ctr_tag, "OsCounterType")) \
                 .setOsSecondsPerTick(self.read_optional_value(ctr_tag, "OsSecondsPerTick")) \
-                
+                .setOsCounterWindowsTimer(self.read_optional_value(ctr_tag, "OsCounterWindowsTimer")) \
+                .setOsWindowsIrqLevel(self.read_optional_value(ctr_tag, "OsWindowsIrqLevel")) \
+                .setOsConstName(self.read_optional_value(ctr_tag, "OsConstName")) \
+                .setOsHwModule(self.read_optional_value(ctr_tag, "OsHwModule"))
 
             self.logger.debug("Read OsCounter <%s>" % counter.getName())
             os.addOsCounter(counter)
@@ -249,16 +274,20 @@ class OsXdmParser(AbstractEbModelParser):
             os_app.setOsTrusted(self.read_value(ctr_tag, "OsTrusted"))
             os_app.setOsApplicationCoreAssignment(self.read_value(ctr_tag, "OsApplicationCoreAssignment"))
             os_app.setOsAppEcucPartitionRef(self.read_ref_value(ctr_tag, "OsAppEcucPartitionRef"))
+            os_app.setOsAppErrorHookStack(self.read_optional_value(ctr_tag, "OsAppErrorHookStack"))
+            os_app.setOsAppShutdownHookStack(self.read_optional_value(ctr_tag, "OsAppShutdownHookStack"))
+            os_app.setOsAppStartupHookStack(self.read_optional_value(ctr_tag, "OsAppStartupHookStack"))
+            os_app.setOsTrustedFunctionName(self.read_optional_value(ctr_tag, "OsTrustedFunctionName"))
 
             for ref in self.read_ref_value_list(ctr_tag, "OsAppAlarmRef"):
                 os_app.addOsAppAlarmRef(ref)
 
             for ref in self.read_ref_value_list(ctr_tag, "OsAppCounterRef"):
                 os_app.addOsAppCounterRefs(ref)
-            
+
             for ref in self.read_ref_value_list(ctr_tag, "OsAppIsrRef"):
                 os_app.addOsAppIsrRef(ref)
-            
+
             for ref in self.read_ref_value_list(ctr_tag, "OsAppResourceRef"):
                 os_app.addOsAppResourceRef(ref)
 
@@ -325,3 +354,117 @@ class OsXdmParser(AbstractEbModelParser):
             kernel = OsMicrokernel(os, ctr_tag.attrib["name"])
             self.read_mk_memory_protection(ctr_tag, kernel)
             os.setOsMicrokernel(kernel)
+
+    def read_common_published_information(self, element: ET.Element, os: Os):
+        """Parse CommonPublishedInformation container from XDM."""
+        ctr_tag = self.find_ctr_tag(element, "CommonPublishedInformation")
+        if ctr_tag is not None:
+            pub_info = CommonPublishedInformation(os, ctr_tag.attrib["name"])
+            pub_info.setArMajorVersion(self.read_value(ctr_tag, "ArMajorVersion"))
+            pub_info.setArMinorVersion(self.read_value(ctr_tag, "ArMinorVersion"))
+            pub_info.setArPatchVersion(self.read_value(ctr_tag, "ArPatchVersion"))
+            pub_info.setSwMajorVersion(self.read_value(ctr_tag, "SwMajorVersion"))
+            pub_info.setSwMinorVersion(self.read_value(ctr_tag, "SwMinorVersion"))
+            pub_info.setSwPatchVersion(self.read_value(ctr_tag, "SwPatchVersion"))
+            os.setCommonPublishedInformation(pub_info)
+            self.logger.debug("Read CommonPublishedInformation")
+
+    def read_published_information(self, element: ET.Element, os: Os):
+        """Parse PublishedInformation container from XDM."""
+        ctr_tag = self.find_ctr_tag(element, "PublishedInformation")
+        if ctr_tag is not None:
+            pub_info = PublishedInformation(os, ctr_tag.attrib["name"])
+            pub_info.setVendorId(self.read_value(ctr_tag, "VendorId"))
+            pub_info.setArReleaseMajorVersion(self.read_value(ctr_tag, "ArReleaseMajorVersion"))
+            pub_info.setArReleaseMinorVersion(self.read_value(ctr_tag, "ArReleaseMinorVersion"))
+            pub_info.setArReleasePatchVersion(self.read_value(ctr_tag, "ArReleasePatchVersion"))
+            pub_info.setSwMajorVersion(self.read_value(ctr_tag, "SwMajorVersion"))
+            pub_info.setSwMinorVersion(self.read_value(ctr_tag, "SwMinorVersion"))
+            pub_info.setSwPatchVersion(self.read_value(ctr_tag, "SwPatchVersion"))
+            os.setPublishedInformation(pub_info)
+            self.logger.debug("Read PublishedInformation")
+
+    def read_os_hw_incrementer(self, element: ET.Element, os: Os):
+        """Parse OsHwIncrementer container from XDM."""
+        ctr_tag = self.find_ctr_tag(element, "OsHwIncrementer")
+        if ctr_tag is not None:
+            hw_inc = OsHwIncrementer(os, ctr_tag.attrib["name"])
+            hw_inc.setOsHwIncrementerBase(self.read_value(ctr_tag, "OsHwIncrementerBase"))
+            hw_inc.setOsHwIncrementerMax(self.read_value(ctr_tag, "OsHwIncrementerMax"))
+            os.setOsHwIncrementer(hw_inc)
+            self.logger.debug("Read OsHwIncrementer")
+
+    def read_os_events(self, element: ET.Element, os: Os):
+        """Parse all OsEvent containers from XDM."""
+        for ctr_tag in self.find_ctr_tag_list(element, "OsEvent"):
+            event = OsEvent(os, ctr_tag.attrib["name"])
+            event.setOsEventMask(self.read_value(ctr_tag, "OsEventMask"))
+            os.addOsEvent(event)
+            self.logger.debug("Read OsEvent <%s>" % event.getName())
+
+    def read_os_spinlocks(self, element: ET.Element, os: Os):
+        """Parse all OsSpinlock containers from XDM."""
+        for ctr_tag in self.find_ctr_tag_list(element, "OsSpinlock"):
+            spinlock = OsSpinlock(os, ctr_tag.attrib["name"])
+            spinlock.setOsSpinlockType(self.read_value(ctr_tag, "OsSpinlockType"))
+            spinlock.setOsSpinlockSpinCount(self.read_value(ctr_tag, "OsSpinlockSpinCount"))
+            os.addOsSpinlock(spinlock)
+            self.logger.debug("Read OsSpinlock <%s>" % spinlock.getName())
+
+    def read_os_peripheral_areas(self, element: ET.Element, os: Os):
+        """Parse all OsPeripheralArea containers from XDM."""
+        for ctr_tag in self.find_ctr_tag_list(element, "OsPeripheralArea"):
+            area = OsPeripheralArea(os, ctr_tag.attrib["name"])
+            area.setOsPeripheralAreaStartAddress(self.read_value(ctr_tag, "OsPeripheralAreaStartAddress"))
+            area.setOsPeripheralAreaEndAddress(self.read_value(ctr_tag, "OsPeripheralAreaEndAddress"))
+            area.setOsPeripheralAreaAccessPermission(self.read_value(ctr_tag, "OsPeripheralAreaAccessPermission"))
+            os.addOsPeripheralArea(area)
+            self.logger.debug("Read OsPeripheralArea <%s>" % area.getName())
+
+    def read_os_os(self, element: ET.Element, os: Os):
+        """Parse OsOS container from XDM."""
+        ctr_tag = self.find_ctr_tag(element, "OsOS")
+        if ctr_tag is not None:
+            os_os = OsOS(os, ctr_tag.attrib["name"])
+            os_os.setOsOSCoreAssignment(self.read_value(ctr_tag, "OsOSCoreAssignment"))
+            os_os.setOsOsStackMonitoring(self.read_value(ctr_tag, "OsOsStackMonitoring"))
+            os_os.setOsOsUseGetServiceId(self.read_value(ctr_tag, "OsOsUseGetServiceId"))
+            os_os.setOsOsUseParameterAccess(self.read_value(ctr_tag, "OsOsUseParameterAccess"))
+            os_os.setOsOsUseServiceId(self.read_value(ctr_tag, "OsOsUseServiceId"))
+            os.setOsOS(os_os)
+            self.logger.debug("Read OsOS")
+
+    def read_os_hooks(self, element: ET.Element, os: Os):
+        """Parse OsHooks container from XDM."""
+        ctr_tag = self.find_ctr_tag(element, "OsHooks")
+        if ctr_tag is not None:
+            hooks = OsHooks(os, ctr_tag.attrib["name"])
+            hooks.setOsErrorHook(self.read_value(ctr_tag, "OsErrorHook"))
+            hooks.setOsShutdownHook(self.read_value(ctr_tag, "OsShutdownHook"))
+            hooks.setOsStartupHook(self.read_value(ctr_tag, "OsStartupHook"))
+            hooks.setOsPreTaskHook(self.read_value(ctr_tag, "OsPreTaskHook"))
+            hooks.setOsPostTaskHook(self.read_value(ctr_tag, "OsPostTaskHook"))
+            hooks.setOsProtectionHook(self.read_value(ctr_tag, "OsProtectionHook"))
+            os.setOsHooks(hooks)
+            self.logger.debug("Read OsHooks")
+
+    def read_os_core_configs(self, element: ET.Element, os: Os):
+        """Parse all OsCoreConfig containers from XDM."""
+        for ctr_tag in self.find_ctr_tag_list(element, "OsCoreConfig"):
+            core_config = OsCoreConfig(os, ctr_tag.attrib["name"])
+            core_config.setOsCoreId(self.read_value(ctr_tag, "OsCoreId"))
+            core_config.setOsCoreMainFunction(self.read_value(ctr_tag, "OsCoreMainFunction"))
+            core_config.setOsCoreStackStartAddress(self.read_value(ctr_tag, "OsCoreStackStartAddress"))
+            core_config.setOsCoreStackSize(self.read_value(ctr_tag, "OsCoreStackSize"))
+            os.addOsCoreConfig(core_config)
+            self.logger.debug("Read OsCoreConfig <%s>" % core_config.getName())
+
+    def read_os_autosar_customization(self, element: ET.Element, os: Os):
+        """Parse OsAutosarCustomization container from XDM."""
+        ctr_tag = self.find_ctr_tag(element, "OsAutosarCustomization")
+        if ctr_tag is not None:
+            customization = OsAutosarCustomization(os, ctr_tag.attrib["name"])
+            customization.setOsScalableClass(self.read_value(ctr_tag, "OsScalableClass"))
+            customization.setOsApplicationType(self.read_value(ctr_tag, "OsApplicationType"))
+            os.setOsAutosarCustomization(customization)
+            self.logger.debug("Read OsAutosarCustomization")
